bcfe0091275826317e0a92ba7f3c7775fb9850c8
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2022-05-17 23:00:46 +0200

    styling

--- src/test/java/com/foo/ReproducerTest.java
+++ src/test/java/com/foo/ReproducerTest.java
@@ -20,6 +20,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ReproducerTest extends BottleRocketTest {
+    public static final String TESTING_ID = "testing_object";
     private Datastore datastore;
 
     public ReproducerTest() {
@@ -43,38 +44,42 @@ public class ReproducerTest extends BottleRocketTest {
 
     @Test
     public void reproduce() {
-        SaveData toSave = new SaveData("testing_object", new double[]{1,2,3}, List.of(
+        trySave();
+        tryLoad();
+    }
+
+    public void trySave() {
+        SaveData toSave = new SaveData(TESTING_ID, new double[]{1,2,3}, List.of(
                 new SaveData.ChildData(new double[]{4,5,6}, new ArrayList<>()),
                 new SaveData.ChildData(new double[]{7,8,9}, new ArrayList<>()),
                 new SaveData.ChildData(new double[]{10,11,12}, new ArrayList<>())
         ));
-        //System.out.println(toSave);
-        //System.out.println(toSave.hashCode());
 
-        //make sure the child objects are not the same
+        // Make sure the child objects are not the same
         assertNotSame(toSave.elements.get(0), toSave.elements.get(1));
         assertNotSame(toSave.elements.get(0), toSave.elements.get(2));
         assertNotSame(toSave.elements.get(1), toSave.elements.get(2));
 
-        //make sure the child position arrays are not the same
+        // Make sure the child position arrays are not the same
         assertFalse(Arrays.equals(toSave.elements.get(0).child_position, toSave.elements.get(1).child_position));
         assertFalse(Arrays.equals(toSave.elements.get(0).child_position, toSave.elements.get(2).child_position));
         assertFalse(Arrays.equals(toSave.elements.get(1).child_position, toSave.elements.get(2).child_position));
 
         datastore.save(toSave);
+    }
 
+    public void tryLoad() {
+        SaveData loadedData = datastore.find(SaveData.class).filter(Filters.eq("_id", TESTING_ID)).first();
 
-        SaveData loadedData = datastore.find(SaveData.class).filter(Filters.eq("_id", "testing_object")).first();
+        // Make sure the query actually worked
+        assertNotNull(loadedData);
 
-        //System.out.println(loadedData);
-        //System.out.println(loadedData.hashCode());
-
-        //make sure the child objects are not the same
+        // Make sure the child objects are not the same
         assertNotSame(loadedData.elements.get(0), loadedData.elements.get(1));
         assertNotSame(loadedData.elements.get(0), loadedData.elements.get(2));
         assertNotSame(loadedData.elements.get(1), loadedData.elements.get(2));
 
-        //make sure the child position arrays are not the same
+        // Make sure the child position arrays are not the same
         assertFalse(Arrays.equals(loadedData.elements.get(0).child_position, loadedData.elements.get(1).child_position));
         assertFalse(Arrays.equals(loadedData.elements.get(0).child_position, loadedData.elements.get(2).child_position));
         assertFalse(Arrays.equals(loadedData.elements.get(1).child_position, loadedData.elements.get(2).child_position));
